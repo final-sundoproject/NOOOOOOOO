bc552e0519bcbf190a1338acd4a5e9723fb33e7d
junwon-hwang <nyong1320@gmail.com>
2024-08-11 21:58:14 +0900

    " 맵에서 연결 "

--- app/src/main/java/com/example/sundo_project_app/EvaluationFindAllActivity.java
+++ app/src/main/java/com/example/sundo_project_app/EvaluationFindAllActivity.java
@@ -1,5 +1,6 @@
 package com.example.sundo_project_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
@@ -17,6 +18,7 @@ import org.json.JSONObject;
 public class EvaluationFindAllActivity extends AppCompatActivity {
 
     private Button btnShowDialog;
+    private Button btnShowList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +26,17 @@ public class EvaluationFindAllActivity extends AppCompatActivity {
         setContentView(R.layout.map);
 
         btnShowDialog = findViewById(R.id.btnShowDialog);
+        btnShowList = findViewById(R.id.enteredPoint);
 
         btnShowDialog.setOnClickListener(v -> {
-            Log.d("EvaluationFindAllActivity", "점수목록 버튼 클릭됨");
+            Log.d("EvaluationFindAllActivity", "평가리스트 버튼 클릭됨");
             fetchDataAndShowDialog();
+        });
+
+        btnShowList.setOnClickListener(v -> {
+            Log.d("btnShowList", "평가입력 버튼 클릭됨");
+            Intent intent = new Intent(EvaluationFindAllActivity.this, EvaluationActivity.class);
+            startActivity(intent);
         });
     }
 
