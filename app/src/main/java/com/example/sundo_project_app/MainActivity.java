2f916d20d20fcb23c083914b6c1177fbdef99c73
junwon-hwang <nyong1320@gmail.com>
2024-08-13 11:20:11 +0900

    "앱실행 권한 부여 "

--- app/src/main/java/com/example/sundo_project_app/MainActivity.java
+++ app/src/main/java/com/example/sundo_project_app/MainActivity.java
@@ -1,25 +1,65 @@
 package com.example.sundo_project_app;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
+        // 권한 확인 및 요청
+        if (hasPermission()) {
+            setContentView(R.layout.activity_main);
+            navigateToLoginActivity();
+        } else {
+            requestPermission();
+        }
+    }
+
+    private boolean hasPermission() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 권한이 허용된 경우
+                setContentView(R.layout.activity_main);
+                navigateToLoginActivity();
+            } else {
+                // 권한이 거부된 경우
+                Toast.makeText(this, "권한이 거부되었습니다. 앱을 종료합니다.", Toast.LENGTH_LONG).show();
+                finish();  // 앱 종료
             }
+        }
+    }
+
+    private void navigateToLoginActivity() {
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
         }, 1500);
     }
 }
