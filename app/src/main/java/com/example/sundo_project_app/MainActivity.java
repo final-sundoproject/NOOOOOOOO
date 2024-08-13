44d113c18b44d13c9179355c7267af8f830b174f
junwon-hwang <nyong1320@gmail.com>
2024-08-13 14:07:10 +0900

    "앱실행 권한 부여2 "

--- app/src/main/java/com/example/sundo_project_app/MainActivity.java
+++ app/src/main/java/com/example/sundo_project_app/MainActivity.java
@@ -1,65 +1,95 @@
 package com.example.sundo_project_app;
 
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int PERMISSION_REQUEST_CODE = 1;
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 1;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // 권한 확인 및 요청
-        if (hasPermission()) {
-            setContentView(R.layout.activity_main);
-            navigateToLoginActivity();
+        if (hasCameraPermission()) {
+            if (hasLocationPermission()) {
+                setContentView(R.layout.activity_main);
+            } else {
+                requestLocationPermission();
+            }
         } else {
-            requestPermission();
+            showPermissionExplanationDialog();
         }
     }
 
-    private boolean hasPermission() {
+    private boolean hasCameraPermission() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private boolean hasLocationPermission() {
         return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
     }
 
-    private void requestPermission() {
-        ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                PERMISSION_REQUEST_CODE);
+
+
+    private void showPermissionExplanationDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.dialog_permission_explanation, null);
+        builder.setView(view);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        Button allow = view.findViewById(R.id.btn_allow);
+        Button deny = view.findViewById(R.id.btn_deny);
+
+        allow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+
+            }
+        });
+
+        deny.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "카메라 권한이 거절되었습니다. \n앱을 종료합니다.", Toast.LENGTH_LONG).show();
+                dialog.dismiss();
+                finish(); // 앱 종료
+            }
+        });
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == PERMISSION_REQUEST_CODE) {
+
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // 권한이 허용된 경우
-                setContentView(R.layout.activity_main);
-                navigateToLoginActivity();
+                requestLocationPermission(); // 카메라 권한 승인 시 위치 권한 요청
             } else {
-                // 권한이 거부된 경우
-                Toast.makeText(this, "권한이 거부되었습니다. 앱을 종료합니다.", Toast.LENGTH_LONG).show();
-                finish();  // 앱 종료
+                Toast.makeText(this, "카메라 권한이 거부되었습니다. \n앱을 종료합니다.", Toast.LENGTH_LONG).show();
+                finish(); // 앱 종료
+            }
+        } else if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                setContentView(R.layout.activity_main); // 모든 권한이 승인되었을 때 UI 설정
+            } else {
+                Toast.makeText(this, "위치 권한이 거부되었습니다. \n앱을 종료합니다.", Toast.LENGTH_LONG).show();
+                finish(); // 앱 종료
             }
         }
-    }
-
-    private void navigateToLoginActivity() {
-        new Handler().postDelayed(() -> {
-            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-            startActivity(intent);
-            finish();
-        }, 1500);
     }
 }
