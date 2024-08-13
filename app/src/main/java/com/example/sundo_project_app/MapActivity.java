dde2cc71d4e3d1f44466428bab7c699142d79fc1
kimbyeongmo <kbmomo9999@naver.com>
2024-08-13 10:56:05 +0900

    # css(제외) DMS,DD 좌표 값 저장 완료

--- app/src/main/java/com/example/sundo_project_app/MapActivity.java
+++ app/src/main/java/com/example/sundo_project_app/MapActivity.java
@@ -3,6 +3,7 @@ package com.example.sundo_project_app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MapActivity extends AppCompatActivity {
@@ -12,13 +13,12 @@ public class MapActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map);
 
-        // '좌표 선택' 버튼을 찾고 클릭 리스너를 추가합니다.
+        // '좌표 선택' 버튼 클릭 시 ChoiceCooridate 모달을 표시합니다.
         findViewById(R.id.coordinateInput).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // DmsActivity 시작합니다.
-                Intent intent = new Intent(MapActivity.this, DmsActivity.class);
-                startActivity(intent);
+                ChoiceCooridate choiceCoordinateDialog = new ChoiceCooridate();
+                choiceCoordinateDialog.show(getSupportFragmentManager(), "choiceCoordinateDialog");
             }
         });
 
